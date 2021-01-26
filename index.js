57034518e9448c971bb0f0ec43baff6d04659b62
Michael W <oculartide@gmail.com>
2021-01-26 02:11:52 -0600

    Update index.js
    Added basic ability to insert records, and generate SQL for committing to database. type checking not yet implemented. As a result not all queries will be properly formatted.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ Written by Michael Warner. 2021
 
 "use strict";
 
+const sqlite = require('sqlite3');
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -30,6 +32,7 @@ const DataTypes = {
 	'TEXT': String.prototype,
 	'CLOB': String.prototype,
 	'BLOB': String.prototype,
+	'BLOB': String.prototype,
 	
 	'REAL': Number.prototype,
 	'DOUBLE': Number.prototype,
@@ -47,7 +50,8 @@ const DataTypes = {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class SQLiteMason
 {
-	constructor () {
+	constructor (path) {
+		this.db = new sqlite.Database(path);
 		this.cache = {};
 		this.cache.tables = {};
 		this.cache.edited = false;
@@ -72,6 +76,103 @@ class SQLiteMason
 	
 	//////////////////////////////////////////////////////////////////////////////////////////
 	
+	runSQL(query) {
+		return this.db.run(query);
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////
+	
+	getSQL(query) {
+		return this.db.get(query);
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////
+	
+	getTableFieldSQL(tableName, array) {
+		if (this.cache.tables[tableName] == undefined && array == undefined) return '';
+		const table = (array == undefined) ? this.cache.tables[tableName] : array; // Use provided array instead of stored, if available.
+		
+		const prefix = 'CREATE TABLE IF NOT EXISTS [' + tableName + '] (\n';
+		const suffix = ');';
+	
+		let middle = ''
+		
+		let fieldNames = this.cache.tables[tableName].keys;
+		
+		for(let i = 0; i < fieldNames.length; i++)
+		{
+			const fieldName = fieldNames[i]
+			const row = table.fields[fieldName];
+			
+			let newSql = '    [' + fieldName + '] ' + row.type;
+			
+			if(row.primary) newSql += ' PRIMARY KEY ON CONFLICT ABORT'; // Begin adding flags.
+			if(row.notNull) newSql += ' NOT NULL';
+			if(row.unique) newSql += ' UNIQUE';
+			if(row.defaultVal != '' && row.unique == false) newSql += ` DEFAULT ${row.defaultVal}`; 
+			
+			// Appends comma if we have another row and always a newline escape sequence.
+			middle += newSql + ((i < fieldNames.length - 1) ? ',\n' : ''); // Sometimes you write something and it seems like a great idea,
+			                                                              // until you look back on it and go, "Dear lord." and die a little inside.
+		}
+		
+		return prefix + middle + suffix;
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////
+	// WIP definitely will not work for all data, just as a heads up.
+	
+	getTableRecordSQL(tableName, array) {
+		if(this.cache.tables[tableName] == undefined) return '';
+		
+		////////////////////////////////////////////////////////////
+		
+		const tableFields = this.cache.tables[tableName].keys;
+		const tableFieldData = this.cache.tables[tableName].fields;
+		const tableRecords = (array == undefined) ? this.cache.tables[tableName].records : array;
+		
+		////////////////////////////////////////////////////////////
+		
+		if(tableRecords == []) return ''; // If there are no records we can't do anything.
+		
+		////////////////////////////////////////////////////////////
+		
+		const fieldString = tableFields.join(', ');
+		const prefix = 'INSERT OR REPLACE INTO ' + tableName + ' (' + fieldString + ') VALUES\n';
+		const suffix = ';';
+		
+		let middle = '';
+		
+		////////////////////////////////////////////////////////////
+		
+		for(let i = 0; i < tableRecords.length; i++)
+		{
+			const currentRecord = tableRecords[i]
+			let end = (i < tableRecords.length - 1) ? '),\n' : ')';
+			
+			let elements = '(';
+			
+			/////////////////////////////////////////////////
+			
+			for(let e = 0; e < tableFields.length; e++)
+			{
+				elements += currentRecord[tableFields[e]];
+				
+				if (e < tableFields.length - 1) elements += ',';
+			}
+			
+			/////////////////////////////////////////////////
+			
+			middle += elements + end + '';
+		}
+		
+		////////////////////////////////////////////////////////////
+		
+		return prefix + middle + suffix;
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////
+	
 	addTable(tableName)
 	{
 		if(this.cache.tables[tableName] != undefined) return false;
@@ -86,6 +187,21 @@ class SQLiteMason
 		//////////////////////////////
 		
 		return true;
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////
+	
+	commitTable(tableName, array) {
+		if(array != undefined) this.cache.tables[tableName] = array;
+		this.runSQL(this.getTableFieldSQL(tableName));
+		
+		return true;
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////
+	
+	commitTableRecords(tableName, array) {
+		
 	}
 	
 	//////////////////////////////////////////////////////////////////////////////////////////
@@ -126,7 +242,7 @@ class SQLiteMason
 		return true;
 	}
 	
-	////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////
 	
 	addRecord(tableName, data) {
 		if(this.cache.tables[tableName] == undefined) return false; // Ensure table exists.
@@ -142,52 +258,54 @@ class SQLiteMason
 		return true;
 	}
 	
-	////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////
 	
 	getTableJSON(tableName) {
 		return JSON.stringify(this.cache.tables[tableName]);
+		
 	}
 	
-	////////////////////////////////////////////////////////////
 	
-	getTableFieldSQL(tableName) {
-		if (this.cache.tables[tableName] == undefined) return '';
-		const table = this.cache.tables[tableName];
-		
-		const prefix = 'CREATE TABLE IF NOT EXISTS [' + tableName + '] (\n';
-		const suffix = ');';
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+const colors = require('colors');
+
+let testdb = new SQLiteMason('./test.sqlite');
+
+let testResults = {};
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+function checkTestResults (testResults) {
 	
-		let middle = ''
+	let keys = Object.keys(testResults);
+	
+	for(let i = 0; i < keys.length; i++)
+	{
+		let name = keys[i].match(/[A-Z][a-z]+|[0-9]+/g).join(' ');
 		
-		let fieldNames = this.cache.tables[tableName].keys;
-		
-		for(let i = 0; i < fieldNames.length; i++)
-		{
-			const fieldName = fieldNames[i]
-			const row = table.fields[fieldName];
-			
-			let newSql = '    [' + fieldName + '] ' + row.type;
-			
-			if(row.primary) newSql += ' PRIMARY KEY ON CONFLICT ABORT'; // Begin adding flags.
-			if(row.notNull) newSql += ' NOT NULL';
-			if(row.unique) newSql += ' UNIQUE';
-			if(row.defaultVal != '' && row.unique == false) newSql += ` DEFAULT ${row.defaultVal}`; 
-			
-			// Appends comma if we have another row and always a newline escape sequence.
-			middle += newSql + ((i < fieldNames.length - 1) ? ',\n' : ''); // Sometimes you write something and it seems like a great idea,
-			                                                              // until you look back on it and go, "Dear lord." and die a little inside.
-		}
-		
-		return prefix + middle + suffix;
+		console.log(colors.yellow('Test ') + name + ': ' + ((testResults[keys[i]]) ? colors.green('PASS'): colors.red('FAIL')));
 	}
 }
 
-let testdb = new SQLiteMason();
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
 
-console.log(testdb.addTable('users'));
-console.log(testdb.addTableField('users', { name: 'internal_id', type: 'BIGINT', unique: true, primary: true, notNull: true }));
-console.log(testdb.addTableField('users', { name: 'discord_id', type: 'BLOB_TEXT', unique: true, notNull: true }));
-console.log(testdb.addTableField('users', { name: 'sys_admin', type: 'BOOLEAN', notNull: true, defaultVal: '0'}));
-console.log(testdb.addRecord('users', { 'internal_id': '0', 'discord_id': '0', 'sys_admin': false}));
+console.log('Beginning Self-test, false positives may be abound...');
 
-console.log(testdb.getTableFieldSQL('users'));
+testResults.CreateTable1 = testdb.addTable('users');
+testResults.CreateField1 = testdb.addTableField('users', { name: 'internal_id', type: 'BIGINT', unique: true, primary: true, notNull: true });
+testResults.CreateField2 = testdb.addTableField('users', { name: 'discord_id', type: 'BLOB_TEXT', unique: true, notNull: true });
+testResults.CreateField3 = testdb.addTableField('users', { name: 'sys_admin', type: 'BOOLEAN', notNull: true, defaultVal: '0'});
+testResults.AddRecord1 = testdb.addRecord('users', { 'internal_id': '12312', 'discord_id': '43242341235452354', 'sys_admin': false});
+testResults.AddRecord2 = testdb.addRecord('users', { 'internal_id': '12313', 'discord_id': '00002341235452354', 'sys_admin': false});
+
+testResults.CommitTable1 = testdb.commitTable('users');
+
+console.log(testdb.getTableRecordSQL('users'));
+
+checkTestResults(testResults);
